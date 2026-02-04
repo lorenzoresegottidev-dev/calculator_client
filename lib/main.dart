d2b0590992eed18a3d0480554d926f9233619607
lorenzoresegottidev-dev <lorenzo.resegotti.dev@gmail.com>
2026-02-04 09:44:59 +0100

    first implementation

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:calculator_plugin/calculator_plugin.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    IOperations op = Operations();
+    String sum = op.somma(3, 5).toString();
+    String greet = op.greet();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(greet + "\n" + sum),
+      ),
+    );
+  }
+}
